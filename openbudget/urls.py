536783f8c4246fbf273b4d4889e8776e6ddd3d06
Jerzy de Mezer <jerzydem@gmail.com>
2011-08-24 15:51:32 +0200

    rebuild css, add content

--- openbudget/urls.py
+++ openbudget/urls.py
@@ -15,4 +15,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('openbudget.budget_site.urls')),
+    url(r'^tiny_mce/(?P<path>.*)$', 'django.views.static.serve',
+                 { 'document_root': '/mymedia/js/tiny_mce/' }),
+    url(r'', include('django.contrib.flatpages.urls')),
 )
